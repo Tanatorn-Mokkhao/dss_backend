ec79deb264000cfc0b26df464e6d6c85f6c93c42
Tanatorn-Mokkhao <6106021621041@fitm.kmutnb.ac.th>
2021-04-12 13:49:56 +0700

    update commit

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(
   cors({
     credentials: true,
     // origin: "http://localhost:3000",
-    origin: "https://dssfontend.herokuapp.com/",
+    origin: "https://dssfontend.herokuapp.com",
   })
 );
 
