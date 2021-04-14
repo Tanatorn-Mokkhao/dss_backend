c7c1c41c9b0f7cb1f8a4e46f76551aa68f72ba2c
Tanatorn-Mokkhao <6106021621041@fitm.kmutnb.ac.th>
2021-04-14 15:12:45 +0700

    update commit

--- src/controller/user.js
+++ src/controller/user.js
@@ -47,7 +47,7 @@ exports.signin = (req, res) => {
             // sameSite: "none",
             // secure: true,
             sameSite: "strict",
-            secure: true,
+            // secure: true,
             path: "/",
             expires: new Date(new Date().getTime() + 60 * 60 * 24 * 1000),
             httpOnly: true,
